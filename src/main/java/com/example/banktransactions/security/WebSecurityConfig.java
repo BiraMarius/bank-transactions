b0f69774d07dc4bf527c424ccbd6d2a660bded8f
MariusB <bira_marius_gabriel@yahoo.com>
2024-07-19 18:42:57 +0300

    update at registration form

---
+++ src/main/java/com/example/banktransactions/security/WebSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.banktransactions.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/","/home","/login","/register").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout((logout) -> logout.permitAll());
+        return http.build();
+    }
+
+}
